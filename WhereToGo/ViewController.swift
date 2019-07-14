fcbbc4493f402dcc17c493ad6adf0d95112c731f
Дмитрий Ага <royalthings2019@gmail.com>
2019-07-14 15:45:10 +0300

    Initial Commit

---
+++ WhereToGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhereToGo
+//
+//  Created by Дмитрий Ага on 7/14/19.
+//  Copyright © 2019 Дмитрий Ага. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
